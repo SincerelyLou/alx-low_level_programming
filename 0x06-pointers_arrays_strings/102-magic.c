735ba02ddb70acf142706220d68601d7660ad40e
SincerelyLou <louisakpabi@gmail.com>
2023-05-05 09:42:53 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 int main(void)
 {
 int n;
@@ -11,12 +11,12 @@ p = &n;
   /*
    * write your line of code here...
    * Remember:
-   * - you are not allowed to use the variable a
-   * - you are not allowed to modify the variable p
-   * - you can only write one statement
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-p[5] = 98;
+*(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
