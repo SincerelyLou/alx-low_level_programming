ed445e9126752ede879ac15da5e37e06e295fc26
SincerelyLou <louisakpabi@gmail.com>
2023-05-10 05:35:44 -0700

    advanced task 2

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compare two strings,considering wildcard characters
+ * @s1: the first string to be compared
+ * @s2: the second string to be compared
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
+
